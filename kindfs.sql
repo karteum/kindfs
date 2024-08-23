51dfeffc7d5e9a9bb09aaa1f13a3197842f720f0
Adrien Demarez <adrien.demarez@free.fr>
2024-08-24 01:22:10 +0200

    Current schema

--- kindfs.sql
+++ kindfs.sql
@@ -1,28 +1,65 @@
-drop table if exists files;
-create table files(
+PRAGMA user_version = 2;
+
+drop table if exists entries;
+create table entries(
     id integer primary key autoincrement,
-    parentdir text,
-    name text,
+    type CHAR(1) NOT NULL,
+    path text UNIQUE NOT NULL,
+    parentdir_len integer,
+    parentdir text GENERATED ALWAYS AS (substr(path,1,parentdir_len)) VIRTUAL,
+    name text GENERATED ALWAYS AS (substr(path,parentdir_len+iif(parentdir_len<2,1,2))) VIRTUAL,
+    depht integer GENERATED ALWAYS AS (length(path)-length(replace(path,'/',''))-1) VIRTUAL,
     size integer,
-    xxh64be integer,
-    st_mtime integer, st_mode integer, st_uid integer, st_gid integer, st_ino integer, st_nlink integer, st_dev integer,
-    sqlite_entry_created integer
-);
-create index files_name_idx on files(name);
-create index files_size_idx on files(size);
-create index files_xxh64be_idx on files(xxh64be);
-drop table if exists dirs;
-create table dirs(
-    id integer primary key autoincrement,
-    parentdir text,
-    name text,
-    size integer,
-    nsubfiles integer,
+    hash integer,
+    magictype integer,
     nsubdirs integer,
-    xxh64be integer,
-    st_mtime integer, st_mode integer, st_uid integer, st_gid integer, st_dev integer,
-    sqlite_entry_created integer
+    nsubfiles integer,
+    nsubfiles_rec integer,
+    symtarget text,
+    st_mtime integer, st_mode integer, st_uid integer, st_gid integer, st_ino integer, st_nlink integer, st_dev integer,
+    dbsession integer not null
 );
-create index dirs_name_idx on dirs(name);
-create index dirs_size_idx on dirs(size);
-create index dirs_xxh64be_idx on dirs(xxh64be);
+create index entries_parentdir_idx on entries(parentdir);
+create index entries_path_idx on entries(path);
+create index entries_name_idx on entries(name);
+create index entries_size_idx on entries(size);
+create index entries_hash_idx on entries(hash);
+create index entries_depht_idx on entries(depht);
+
+drop view if exists files;
+create view files as select id,parentdir,name,path,size,hash as xxh64be,st_mtime, st_mode, st_uid, st_gid, st_ino, st_nlink, st_dev,dbsession,magictype from entries where type='F';
+drop view if exists dirs;
+create view dirs as select id,parentdir,name,path,size,nsubfiles,nsubdirs,hash as xxh64be,st_mtime, st_mode, st_uid, st_gid, st_nlink, st_dev,dbsession,magictype from entries where type='D';
+drop view if exists symlinks;
+create view symlinks as select id,parentdir,name,path,symtarget as target,NULL as type,hash as xxh64be,dbsession,magictype from entries where type='S';
+
+drop table if exists dbsessions;
+create table dbsessions(
+    id integer primary key autoincrement,
+    timestamp integer not null,
+    init_path text
+);
+
+drop table if exists magictypes;
+create table magictypes(
+    id integer primary key autoincrement,
+    magicmime text,
+    magictype text
+);
+create index magictypes_magictype_idx on magictypes(magictype);
+
+drop table if exists postops;
+create table postops (
+    id integer primary key autoincrement,
+    op text,
+    parentdir text,
+    path text,
+    arg text
+);
+create index postops_parentdir_idx on postops(parentdir);
+create index postops_path_idx on postops(path);
+
+PRAGMA main.page_size=4096;
+PRAGMA main.cache_size=10000;
+PRAGMA main.locking_mode=EXCLUSIVE;
+PRAGMA main.synchronous=NORMAL;
